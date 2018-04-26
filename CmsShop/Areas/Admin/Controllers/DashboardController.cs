bd16b0490a01e1d8a0f5594939cfa6117a848c65
mariusz jurczenko <mariuszjurczenko@gmail.com>
2018-04-26 10:10:29 +0200

    Dashboard Admin

---
+++ CmsShop/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace CmsShop.Areas.Admin.Controllers
+{
+    public class DashboardController : Controller
+    {
+        // GET: Admin/Dashboard
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
