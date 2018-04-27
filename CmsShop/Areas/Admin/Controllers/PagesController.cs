bc3a8e8d6a53349e54c45bdba337c8c7bd03cb4c
mariusz jurczenko <mariuszjurczenko@gmail.com>
2018-04-27 09:34:14 +0200

    Admin Pages czesc.2

--- CmsShop/Areas/Admin/Controllers/PagesController.cs
+++ CmsShop/Areas/Admin/Controllers/PagesController.cs
@@ -1,4 +1,8 @@
-﻿using System.Web.Mvc;
+﻿using CmsShop.Models.Data;
+using CmsShop.Models.ViewModels.Pages;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
 
 namespace CmsShop.Areas.Admin.Controllers
 {
@@ -7,7 +11,19 @@ namespace CmsShop.Areas.Admin.Controllers
         // GET: Admin/Pages
         public ActionResult Index()
         {
-            return View();
+            // Deklaracja listy PageVM
+            List<PageVM> pagesList;
+
+           
+            using (Db db = new Db())
+            {
+                // Inicjalizacja listy
+                pagesList = db.Pages.ToArray().OrderBy(x => x.Sorting).Select(x => new PageVM(x)).ToList();
+            }
+
+
+            // zwracamy strony do widoku
+            return View(pagesList);
         }
     }
 }
