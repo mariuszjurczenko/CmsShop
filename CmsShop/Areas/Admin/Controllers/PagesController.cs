d047443f6b160bbcf4818ddb7f89c887d6ab5f2e
mariusz jurczenko <mariuszjurczenko@gmail.com>
2018-04-27 10:04:33 +0200

    Admin Pages czesc.3

--- CmsShop/Areas/Admin/Controllers/PagesController.cs
+++ CmsShop/Areas/Admin/Controllers/PagesController.cs
@@ -25,5 +25,11 @@ namespace CmsShop.Areas.Admin.Controllers
             // zwracamy strony do widoku
             return View(pagesList);
         }
+
+        // GET: Admin/Pages/AddPage
+        public ActionResult AddPage()
+        {
+            return View();
+        }
     }
 }
--- CmsShop/Models/ViewModels/Pages/PageVM.cs
+++ CmsShop/Models/ViewModels/Pages/PageVM.cs
@@ -23,12 +23,16 @@ namespace CmsShop.Models.ViewModels.Pages
         public int Id { get; set; }
         [Required]
         [StringLength(50, MinimumLength =3)]
+        [Display(Name = "Tytuł Strony")]
         public string Title { get; set; }
+        [Display(Name = "Adres Strony")]
         public string Slug { get; set; }
         [Required]
         [StringLength(int.MaxValue, MinimumLength = 3)]
+        [Display(Name = "Zawartość Strony")]
         public string Body { get; set; }
         public int Sorting { get; set; }
+        [Display(Name = "Pasek Boczny")]
         public bool HasSidebar { get; set; }
     }
 }
