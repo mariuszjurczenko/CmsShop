bbed304b51c0005cdba3bb5be3ce51946c23a7cc
mariusz jurczenko <mariuszjurczenko@gmail.com>
2018-05-15 09:45:16 +0200

    Admin Shop czesc.6 Sortowanie i usuwanie kategorii

--- CmsShop/Areas/Admin/Controllers/ShopController.cs
+++ CmsShop/Areas/Admin/Controllers/ShopController.cs
@@ -55,5 +55,52 @@ namespace CmsShop.Areas.Admin.Controllers
 
             return id;
         }
+
+        // POST: Admin/Shop/ReorderCategories
+        [HttpPost]
+        public ActionResult ReorderCategories(int[] id)
+        {
+            using (Db db = new Db())
+            {
+                // inicjalizacja licznika
+                int count = 1;
+
+                // deklaracja DTO
+                CategoryDTO dto;
+
+                // sortowanie kategorii
+                foreach (var catId in id)
+                {
+                    dto = db.Categories.Find(catId);
+                    dto.Sorting = count;
+
+                    // zapis na bazie
+                    db.SaveChanges();
+
+                    count++;
+                }
+            }
+
+            return View();
+        }
+
+        // GET: Admin/Shop/DeleteCategory
+        [HttpGet]
+        public ActionResult DeleteCategory(int id)
+        {
+            using (Db db = new Db())
+            {
+                // pobieramy kategorie o podnym id
+                CategoryDTO dto = db.Categories.Find(id);
+
+                // usuwamy kategorie
+                db.Categories.Remove(dto);
+
+                // zapis na bazie
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Categories");
+        }
     }
 }
