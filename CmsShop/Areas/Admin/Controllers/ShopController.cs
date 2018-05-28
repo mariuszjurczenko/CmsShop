718a19e420392adb597257a44b61452e29ab6865
mariusz jurczenko <mariuszjurczenko@gmail.com>
2018-05-28 13:31:19 +0200

    Admin Shop czesc.21 galera zdjec usuwanie. Dodanie przykladowych galerii

--- CmsShop/Areas/Admin/Controllers/ShopController.cs
+++ CmsShop/Areas/Admin/Controllers/ShopController.cs
@@ -512,5 +512,19 @@ namespace CmsShop.Areas.Admin.Controllers
 
             return View();
         }
+
+        // POST: Admin/Shop/DeleteImage
+        [HttpPost]
+        public void DeleteImage(int id, string imageName)
+        {
+            string fullPath1 = Request.MapPath("~/Images/Uploads/Products/" + id.ToString() + "/Gallery/" + imageName);
+            string fullPath2 = Request.MapPath("~/Images/Uploads/Products/" + id.ToString() + "/Gallery/Thumbs/" + imageName);
+
+            if (System.IO.File.Exists(fullPath1))
+                System.IO.File.Delete(fullPath1);
+
+            if (System.IO.File.Exists(fullPath2))
+                System.IO.File.Delete(fullPath2);
+        }
     }
 }
