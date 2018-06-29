e0fc78175fc4a680481f8e7397d7e23f8498e49f
mariusz jurczenko <mariuszjurczenko@gmail.com>
2018-06-29 07:50:21 +0200

    Rejstracja i logowanie czesc.9 obsługa rol i sprawdzanie rol w naszej aplikacji

--- CmsShop/Global.asax.cs
+++ CmsShop/Global.asax.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using CmsShop.Models.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -16,6 +18,34 @@ namespace CmsShop
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Application_AuthenticateRequest()
+        {
+            if (User == null)
+            {
+                return;
+            }
+
+            // pobieramy nazwe uzytkownika
+            string username = Context.User.Identity.Name;
+
+            // deklarujemy tablice z rolami
+            string[] roles = null;
+
+            using (Db db = new Db())
+            {
+                // pobieramy dane dla uzytkownika z bazy aby pobrac role
+                UserDTO dto = db.Users.FirstOrDefault(x => x.UserName == username);
+                roles = db.UserRoles.Where(x => x.UserId == dto.Id).Select(x => x.Role.Name).ToArray();
+            }
+
+            // tworzymy IPrincipal object
+            IIdentity userIdentity = new GenericIdentity(username);
+            IPrincipal newUserObj = new GenericPrincipal(userIdentity, roles);
+
+            // update Context.User
+            Context.User = newUserObj;
         }
     }
 }
