6d03dbf87a9a1d2a2c961b7f1dbfae0165a3e24e
mariusz jurczenko <mariuszjurczenko@gmail.com>
2018-07-03 11:42:30 +0200

    funkcjonalnosc zamowienia cz.5  ordersVM dla uzytkownika i admina

---
+++ CmsShop/Areas/Admin/Models/ViewModels/Shop/OrdersForAdminVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CmsShop.Areas.Admin.Models.ViewModels.Shop
+{
+    public class OrdersForAdminVM
+    {
+        public int OrderNumber { get; set; }
+        public string Username { get; set; }
+        public decimal Total { get; set; }
+        public Dictionary<string, int> ProductsAndQty { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+    }
+}
---
+++ CmsShop/Models/ViewModels/Shop/OrderVM.cs
@@ -0,0 +1,24 @@
+﻿using CmsShop.Models.Data;
+using System;
+
+namespace CmsShop.Models.ViewModels.Shop
+{
+    public class OrderVM
+    {
+
+        public OrderVM()
+        {
+        }
+
+        public OrderVM(OrderDTO row)
+        {
+            OrderId = row.OrderId;
+            UserId = row.UserId;
+            CreatedAt = row.CreatedAt;
+        }
+
+        public int OrderId { get; set; }
+        public int UserId { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
